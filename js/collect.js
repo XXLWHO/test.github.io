3368cec1b89fce45507d786934d7a9fccba60460
XXLWHO <2427018614@qq.com>
2021-05-17 19:30:39 +0800

    18-f

--- js/collect.js
+++ js/collect.js
@@ -1,8 +1,8 @@
 let cancels = document.getElementsByClassName("cancel");
-let collects = document.querySelector(".s_content").children;
-for(let i = 0;i<cancels.length;i++){
+let collects = document.getElementsByClassName("s_content")[0].children;
+for(let i = 0;i<collects.length;i++){
     let cancel = cancels[i];
     cancel.onclick = function(){
-        collects[i].remove();
+        collects[i].style.display ="none"
     }
 }
